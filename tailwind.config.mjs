1e9653040f8dc8e2e872d5e4b525ac7789de9e8c
Erik Bergmann <garfxld@gmail.com>
2025-01-31 10:17:07 +0100

    updated design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+			  blink: {
+				'0%, 45%': { opacity: 1 },
+				'55%, 100%': { opacity: 0 },
+			  },
+			},
+			animation: {
+			  blink: 'blink 1s infinite',
+			},
+		},
 	},
 	plugins: [],
 }
